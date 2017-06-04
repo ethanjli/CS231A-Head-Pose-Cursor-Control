a63deb9c938eb5ce6d90a213aa5e4e3043486ebe
Ethan Li <lietk12@gmail.com>
2017-06-03 19:08:46 -0700

    Actually use ext/StereoVision without depending on pip installation.

--- calibrate.sh
+++ calibrate.sh
@@ -1,1 +1,6 @@
-python ext/StereoVision/build/scripts-2.7/capture_chessboards --rows 6 --columns 8 --square-size 2.54 --calibration-folder calib 0 1 10 calib/images
+cd ext/StereoVision
+python setup.py build
+cp build/scripts-2.7/* build/lib.linux-x86_64-2.7/
+cd build/lib.linux-x86_64-2.7
+python capture_chessboards --rows 6 --columns 8 --square-size 2.54 --calibration-folder ../../../../calib 0 1 20 ../../../../calib/images
+cd ../../../
